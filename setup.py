8d216006a2e775a59d90580e9cd2dcae10e90719
KristofferGus <109553487+mrSWE3@users.noreply.github.com>
2024-07-16 23:36:22 +0200

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package',  # Replace with your package's name
-    version='0.1.0',  # Replace with your package's version
+    name='DevOri',  # Replace with your package's name
+    version='0.0.0',  # Replace with your package's version
     packages=find_packages(),
     include_package_data=True,
-    package_data={'your_package': ['py.typed']},  # Replace 'your_package' with your actual package name
+    package_data={'DevOri': ['py.typed']},  # Replace 'your_package' with your actual package name
 )
